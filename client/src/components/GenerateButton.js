aa502b01429a6b73cfcdb42558e8ceb761239a12
Mukarram Ali <mukarram.ali@thoughtworks.com>
2019-09-17 00:55:10 +0530

    Random generator button populates input field

--- client/src/components/GenerateButton.js
+++ client/src/components/GenerateButton.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export default function GenerateButton() {
-    return <button>
+const SERVICE_API = "http://localhost:8000"
+const GEN_RAND_API = SERVICE_API + "/actions?method=GEN-RAND";
+
+export default function GenerateButton({setRandomNumbers}) {
+    return <button onClick={() => {
+      fetch(GEN_RAND_API)
+      .then(response => response.json())
+      .then(data => setRandomNumbers(data))
+      .catch(err => console.log(err));
+    }}>
         Generate Random Numbers
   </button>
 }
--- client/src/components/RandomNumberField.js
+++ client/src/components/RandomNumberField.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
-export default function RandomNumberField({ randomNumbers, setRandomNumbers }) {
+const RandomNumberField = React.memo(({ randomNumbers, setRandomNumbers }) => {
     let [fieldData, setFieldData] = useState("");
+    useEffect(
+        () => {
+            setFieldData(randomNumbers.join(","));
+        },
+        [randomNumbers],
+    );
     return <div>
         <h3>
             <label htmlFor="random_numbers">
@@ -12,13 +18,15 @@ export default function RandomNumberField({ randomNumbers, setRandomNumbers }) {
                 name="random_numbers"
                 pattern="(\d|\s|,)*"
                 value={fieldData}
-                onChange={({target}) => {
+                onChange={({ target }) => {
                     setFieldData(target.value);
-                    let data = target.value.replace(/\s/g, '');
+                    let data = target.value.replace(/\s*/g, '');
                     setRandomNumbers(data.split(",").filter(e => e))
                 }}
             />
         </h3>
         <p>Enter comma separated list of numbers</p>
     </div>
-}
+}, ({ prevRandomNumbers }, { nextRandomNumbers }) => prevRandomNumbers !== nextRandomNumbers);
+
+export default RandomNumberField;
